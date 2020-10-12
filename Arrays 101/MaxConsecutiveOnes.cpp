01884dc24d72552e92634d21ad43a1a297dba8a4
Kritika Iyer <kiyer@medacuity.local>
2020-10-12 18:11:18 -0400

    Formatting Question presentation

--- Arrays 101/MaxConsecutiveOnes.cpp
+++ Arrays 101/MaxConsecutiveOnes.cpp
@@ -1,14 +1,18 @@
-/* Given a binary array, find the maximum number of consecutive 1s in this array.
+/*
+Given a binary array, find the maximum number of consecutive 1s in this array.
 
 Example 1:
 Input: [1,1,0,1,1,1]
 Output: 3
 Explanation: The first two digits or the last three digits are consecutive 1s.
     The maximum number of consecutive 1s is 3.
-Note:
 
+Contraints:
 The input array will only contain 0 and 1.
-The length of input array is a positive integer and will not exceed 10,000*/
+The length of input array is a positive integer and will not exceed 10,000
+
+*/
+
 #include <vector>
 #include <iostream>
 #include <sstream>
